3d62d69cbecfaa18cef67eaf0192be247f4ab23e
Adam Lisicki <adam.lisicki@adamlisicki.pl>
2023-03-26 22:20:32 +0000

    implement dynamodb into our application

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -44,7 +44,7 @@ class Db:
   # when we want to commit data such as an instert
   # be sure to check for RETURNING in all uppercases
   def query_commit(self,sql,params={}):
-    self.print_sql('commit with returning', sql)
+    self.print_sql('commit with returning', sql, params)
   
     pattern = r"\bRETURNING\b"
     is_returning_id = re.search(pattern, sql)
