2ae45f09322012a808be5363bd5084be1005472e
Adam Lisicki <adam.lisicki@adamlisicki.pl>
2023-04-22 15:27:40 +0000

    render avatar

--- frontend-react-js/src/components/ProfileHeading.js
+++ frontend-react-js/src/components/ProfileHeading.js
@@ -1,5 +1,6 @@
 import './ProfileHeading.css';
 import EditProfileButton from '../components/EditProfileButton';
+import ProfileAvatar from '../components/ProfileAvatar';
 
 export default function ProfileHeading(props) {
   const backgroundImage = 'url("https://assets.cruddur.pl/banners/banner.jpg")';
@@ -12,10 +13,8 @@ export default function ProfileHeading(props) {
   <div className='activity_feed_heading profile_heading'>
     <div className='title'>{props.profile.display_name}</div>
     <div className="cruds_count">{props.profile.cruds_count} Cruds</div>
-    <div className="banner" style={styles} >
-      <div className="avatar">
-        <img src="https://assets.cruddur.pl/avatars/data.jpg"></img>
-      </div>
+    <div className="banner" style={styles} > 
+       <ProfileAvatar id={props.profile.cognito_user_uuid} />
     </div>
     <div className="info">
       <div className='id'>
